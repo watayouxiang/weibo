9a49b7614f3ed61738c20f4346946c0d6849beb8
watayouxiang <watayouxiang@qq.com>
2016-10-18 15:02:58 +0800

    配置项目

--- weibo/weibo/Classes/Main/Controller/wbTabBarController.h
+++ weibo/weibo/Classes/Main/Controller/wbTabBarController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  wbTabBarController.h
 //  weibo
 //
 //  Created by 王涛 on 2016/10/18.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface wbTabBarController : UITabBarController
 
 @end
-
---
+++ weibo/weibo/Classes/Main/Controller/wbTabBarController.m
@@ -0,0 +1,38 @@
+//
+//  wbTabBarController.m
+//  weibo
+//
+//  Created by 王涛 on 2016/10/18.
+//  Copyright © 2016年 wata. All rights reserved.
+//
+
+#import "wbTabBarController.h"
+
+@interface wbTabBarController ()
+
+@end
+
+@implementation wbTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
